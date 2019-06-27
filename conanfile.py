572535fa05afe9f73d206d7f7918614b98fe9bd4
Tom Nelson <tom.nelson@barco.com>
2019-06-27 09:07:03 -0700

    Adding conan packaging

---
+++ conanfile.py
@@ -0,0 +1,25 @@
+from conans import ConanFile, CMake
+
+class LibExpatConan(ConanFile):
+    name = "libexpat"
+    version = "2.2.7"
+    license = "MIT"
+    url = "https://libexpat.github.io/"
+    description = "XML parser"
+    settings = "os", "compiler", "build_type", "arch"
+    options = {}
+    default_options = ""
+    generators = "cmake"
+    exports_sources = "*", "!build/*"
+    
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure(source_folder="expat")
+        cmake.build()
+        cmake.install()
+
+#    def package(self):
+#         nothing to do here. All is handled by cmake.install() above.
+        
+    def package_info(self):
+        self.cpp_info.libs = ["LibExpat"]
